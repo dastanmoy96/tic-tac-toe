2cf2e93283002027e66ea2a1387b1c9f4b90bdb4
Tanmoy Das <dtanmoy2002@gmail.com>
2023-12-01 22:25:10 +0530

    finished with basic game logic

---
+++ scripts/game.js
@@ -0,0 +1,28 @@
+function startNewGame() {
+	if (players[0].name === '' || players[1].name === '') {
+		alert('Please enter custom player names for both players!');
+		return;
+	}
+	
+	activePlayerNameElement.textContent = players[activePlayer].name;
+	gameAreaElement.style.display = 'block';
+}
+
+function switchPlayer() {
+	if (activePlayer === 0) {
+		activePlayer = 1;
+	} else {
+		activePlayer = 0;
+	}
+	activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+	if (event.target.tagName !== 'LI') {
+		return;
+	}
+
+	event.target.textContent = players[activePlayer].symbol; //players[0]
+	event.target.classList.add('disabled');
+	switchPlayer();
+}
